3e1da429285467abb16d1e9588e3c8b029597468
fatma <fk5305774@gmail.com>
2026-09-06 18:25:28 +0300

    feat(firebase): integrate Firebase into the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:bonus_tracker_app/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/theme.dart';
 import 'screens/login_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     DevicePreview(
       enabled: true,
