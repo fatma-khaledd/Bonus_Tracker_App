998da0bde059cb2ab19cd8b966dcab7cfd634801
fatma <fk5305774@gmail.com>
2026-09-13 05:54:57 +0300

    fix(main): switch to device_preview_plus for visible device preview UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bonus_tracker_app/firebase_options.dart';
-import 'package:device_preview/device_preview.dart';
+import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/theme.dart';
 import 'features/auth/screens/login_screen.dart';
@@ -9,8 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  DevicePreview.enable(); // !kReleaseMode already default
-  runApp(const MyApp());
+  runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()));
 }
 
 /// Root application widget configuring global themes and preview settings.
@@ -23,11 +23,15 @@ class MyApp extends StatelessWidget {
       title: 'Mohsens Tracker',
       debugShowCheckedModeBanner: false,
 
+      //Device Preview
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+
       // App Theme Configuration
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.light,
-
+      
       home: const LoginScreen(),
     );
   }
